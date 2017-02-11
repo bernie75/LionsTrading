5602b42ad49b406d4ecc7f40bb327cbd9ed11e91
Yas la menace <yasbernays@gmail.com>
2017-02-11 14:33:19 +0100

    normalize

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Product.create!(sku: 'PETROL', name: '1000-crude-oil WTI', price: 100000 )
+Product.create!(sku: 'PETROL', name: '1000-crude-oil ', price: 100000 )
 Product.create!(sku: 'GOLD', name: '24-carat gold', price: 50000 )
 Product.create!(sku: 'CAR',  name: 'Bentley 4x4', price: 280000 )
+
+Product.create!(sku: 'PETROL',  name: '10-crude-oil', price: 520 )
+Product.create!(sku: 'PETROL', name: '100-crude-oil', price: 5200 )
+Product.create!(sku: 'PETROL', name: 'Tanker-45k-dwt', price: 18000000 )
+
+Product.create!(sku: 'RUBY', name: '1-carat-ruby', price: 3500 )
+Product.create!(sku: 'EMERALD', name: '2-carat-emerald', price: 2900 )
+
+Product.create!(sku: 'DIAMOND', name: '1-carat-uncut-diamond', price: 1500 )
